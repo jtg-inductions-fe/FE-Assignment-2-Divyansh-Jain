27c502a5c766931c55f60854e215c07ff8e29376
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-07 22:30:03 +0530

    Added github action workflow

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,3 @@
-export { Dashboard } from './Dashboard.pages';
-export { NotFoundPage } from './NotFound.pages';
-export { ErrorPage } from './Error.pages';
+export { Dashboard } from './dashboard/Dashboard.pages';
+export { NotFoundPage } from './not-found/NotFound.pages';
+export { ErrorPage } from './error/Error.pages';
