3699f8546bac3350f3a49d9748562e0f9f90387b
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-11 17:54:33 +0530

    DJ_A2_02 : Completed Error page and 404 page

---
+++ src/components/Error/Error.component.tsx
@@ -0,0 +1,56 @@
+import { Box, Stack, useMediaQuery } from '@mui/material';
+
+import { Button, Typography } from '@components';
+import { theme } from '@theme';
+
+import { ErrorComponentProps } from './Error.types';
+
+/**
+ * Error Component
+ *
+ * Displays a user-friendly error message or fallback UI when an error occurs.
+ * Can be used as a boundary fallback, or to handle API errors, 404s, etc.
+ */
+export const ErrorComponent = ({
+    buttonText,
+    imagePath,
+    heading,
+    bodyText,
+    handleButtonClick,
+}: ErrorComponentProps) => {
+    const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+
+    return (
+        <Stack
+            alignItems="center"
+            justifyContent="center"
+            gap={isDesktop ? 0 : 4}
+            height="80%"
+        >
+            <Box>
+                <img src={imagePath} alt="404 Not Found" width="100%" />
+            </Box>
+            <Stack alignItems="center" gap={5.5} px={2}>
+                <Stack alignItems="center" gap={isDesktop ? 0 : 2.5}>
+                    <Typography variant="h1" textAlign="center">
+                        {heading}
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        color={theme.palette.text.secondary}
+                        textAlign="center"
+                    >
+                        {bodyText}
+                    </Typography>
+                </Stack>
+                <Button
+                    variant="contained"
+                    radius={theme.spacing(3)}
+                    onClick={handleButtonClick}
+                >
+                    {buttonText}
+                </Button>
+            </Stack>
+        </Stack>
+    );
+};
