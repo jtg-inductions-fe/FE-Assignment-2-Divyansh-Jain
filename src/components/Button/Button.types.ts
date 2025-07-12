4bfa3c99f8c6beb11805ad697dbcd98df479743a
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-12 12:47:33 +0530

    DJ_A2_02 : Added docstring for Button Component

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,5 +1,6 @@
 import { ButtonProps } from '@mui/material';
 
 export type StyledButtonProps = ButtonProps & {
+    /** String property which takes css units for borderRadius property */
     radius?: string;
 };
