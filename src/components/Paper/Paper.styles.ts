974d015f7a5eb0bbee830bd737ffe5b33866c3ad
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-18 15:01:50 +0530

    DJ_A2_05 : Updated Paper.styles

--- src/components/Paper/Paper.styles.ts
+++ src/components/Paper/Paper.styles.ts
@@ -5,9 +5,13 @@ export const StyledPaper = styled(Paper)<PaperProps>(
         theme: {
             spacing,
             typography: { pxToRem },
+            breakpoints,
         },
     }) => ({
         borderRadius: pxToRem(16),
-        padding: spacing(6),
+        padding: spacing(4),
+        [breakpoints.up('md')]: {
+            padding: spacing(6),
+        },
     }),
 );
