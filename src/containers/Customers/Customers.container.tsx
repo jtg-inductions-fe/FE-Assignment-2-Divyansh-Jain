4a52ef20bf35c8e3648820c148573c5dc10993e6
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-18 12:37:37 +0530

    Work in progress

--- src/containers/Customers/Customers.container.tsx
+++ src/containers/Customers/Customers.container.tsx
@@ -2,25 +2,23 @@ import { List, Stack } from '@mui/material';
 
 import { CardListItem, Paper, Typography } from '@components';
 import { useCustomer } from '@hooks';
-export const Customers = () => {
-    const customerContext = useCustomer();
 
-    // update here
-    const { customers } = customerContext!;
+export const Customers = () => {
+    const { customers } = useCustomer() || {};
 
     return (
         <Paper component="section" sx={{ flexGrow: 1 }}>
             <Stack gap={4}>
                 <Typography variant="h3">Latest Customers</Typography>
                 <List>
-                    {customers.map((customer) => (
+                    {customers?.map((customer) => (
                         <CardListItem
                             key={customer.id}
-                            label={customer.totalSpent.toString()}
-                            primaryText={customer.fullname}
-                            secondaryText={customer.email}
-                            imageUrl={customer.profileUrl}
-                            imageLabel={customer.fullname}
+                            primaryText2={customer.totalSpent.toString()}
+                            primaryText1={customer.fullname}
+                            secondaryText1={customer.email}
+                            src={customer.profileUrl}
+                            alt={customer.fullname}
                         />
                     ))}
                 </List>
--- src/containers/Products/Products.container.tsx
+++ src/containers/Products/Products.container.tsx
@@ -1,13 +1,5 @@
-import { CardListItem, Paper } from '@components';
+import { Paper } from '@components';
 
 export const Products = () => (
-    <Paper component="section" sx={{ flexGrow: 3 }}>
-        <CardListItem
-            imageUrl={''}
-            imageLabel="Bonnie green profile"
-            primaryText="bonnie-green"
-            secondaryText="bonnie@example.com"
-            label="80"
-        />
-    </Paper>
+    <Paper component="section" sx={{ flexGrow: 3 }}></Paper>
 );
