3e0134ff63c1e470cb24f25b7c8bf0a5b050309a
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-18 16:35:54 +0530

    DJ_A2_07 : Added Products.Container

--- src/containers/Products/Products.container.tsx
+++ src/containers/Products/Products.container.tsx
@@ -1,8 +1,28 @@
-// TODO : DJ_A2_00 : Implementation of Products Container
+import { List, Stack } from '@mui/material';
 
-import { Paper } from '@components';
+import { CardListItem, Paper, Typography } from '@components';
+import { useProduct } from '@hooks';
 
-export const Products = () => (
-    // For testing purposes only will be replaced in next PR
-    <Paper component="section" sx={{ flexGrow: 3 }}></Paper>
-);
+export const Products = () => {
+    const { products } = useProduct() || {};
+
+    return (
+        <Paper component="section" sx={{ flexGrow: 3 }}>
+            <Stack gap={4}>
+                <Typography variant="h3">Top Products</Typography>
+                <List>
+                    {products?.map((product, index, arr) => (
+                        <CardListItem
+                            key={product.id}
+                            rightTitle={product.sales.toString()}
+                            rightSubtitle="sales"
+                            leftTitle={product.productName}
+                            leftSubtitle={product.techStack}
+                            showDivider={index < arr.length - 1}
+                        />
+                    ))}
+                </List>
+            </Stack>
+        </Paper>
+    );
+};
