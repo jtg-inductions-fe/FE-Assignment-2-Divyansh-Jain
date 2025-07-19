492f80642d98d73fc96d8df2dbd296218c07962e
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-19 11:37:45 +0530

    Work in progress

--- src/components/CardListItem/CardListItem.component.tsx
+++ src/components/CardListItem/CardListItem.component.tsx
@@ -24,11 +24,16 @@ export const CardListItem = ({
                 py={2}
                 gap={5}
             >
-                <Stack direction="row" alignItems="center" gap={2}>
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    gap={2}
+                    maxWidth="70%"
+                >
                     {avatarSrc && <Avatar src={avatarSrc} alt={avatarAlt} />}
-                    <Box>
+                    <Box minWidth={0} maxWidth="100%">
                         <Tooltip title={leftTitle}>
-                            <Typography variant="subtitle1">
+                            <Typography variant="subtitle1" lines={1}>
                                 {leftTitle}
                             </Typography>
                         </Tooltip>
@@ -36,13 +41,19 @@ export const CardListItem = ({
                             <Typography
                                 variant="subtitle2"
                                 color={palette.text.secondary}
+                                lines={1}
                             >
                                 {leftSubtitle}
                             </Typography>
                         </Tooltip>
                     </Box>
                 </Stack>
-                <Stack direction="row" alignItems="center" gap={1}>
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    gap={1}
+                    maxWidth="30%"
+                >
                     <Tooltip title={rightTitle}>
                         <Typography variant="subtitle1">
                             {rightTitle}
