c688cc323f2adb3ad0a4be7abe05045b7b6c705f
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-19 09:11:11 +0530

    DJ_A2_08 : Added Transaction Table

--- src/context/Transaction/Transaction.types.ts
+++ src/context/Transaction/Transaction.types.ts
@@ -1,15 +1,19 @@
 export type Transaction = {
     /** Unique identifier for the transaction */
     id: string | number;
+
     to?: string;
     from?: string;
-    /** Type REFUND OR TRANSFER */
-    type: string;
+    type: 'CREDIT' | 'DEBIT';
+
+    /** Description or title of the transaction */
+    transaction: string;
+
     /** Current status of the transaction */
-    status: string;
+    status: 'CANCELLED' | 'IN PROGRESS' | 'COMPLETED';
 
     /** Amount involved in the transaction */
-    amount: string;
+    amount: number;
 
     /** Date and time of the transaction */
     dateTime: string;
