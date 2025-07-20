3970e961ce96fc43511fffc56c1073156e229b75
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-20 16:38:08 +0530

    DJ_A2_06 : Updated Customer.container

--- src/containers/Customers/Customers.container.tsx
+++ src/containers/Customers/Customers.container.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, Stack, useTheme } from '@mui/material';
+import { List, ListItem, Stack, useMediaQuery, useTheme } from '@mui/material';
 
 import { CardListItem, Paper, Typography } from '@components';
 import { useCustomer } from '@hooks';
@@ -7,21 +7,26 @@ import { numberFormatter } from '@utilities';
 export const Customers = () => {
     const { customers } = useCustomer() || {};
     const {
+        breakpoints,
         typography: { pxToRem },
     } = useTheme();
+
+    const isDesktop = useMediaQuery(breakpoints.up('md'));
+
     return (
         <Paper
             component="section"
             sx={{
                 flexGrow: 1,
                 minWidth: pxToRem(300),
+                maxWidth: isDesktop ? '40%' : '100%',
             }}
             aria-label="Latest Customers"
         >
             <Stack gap={4}>
                 <Typography variant="h3">Latest Customers</Typography>
                 <List>
-                    {customers ? (
+                    {customers && customers.length > 0 ? (
                         customers?.map((customer, index, arr) => (
                             <CardListItem
                                 key={customer.id}
