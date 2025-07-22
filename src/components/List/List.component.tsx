2088c158ac4c045da1f153d4541eb398f8b55559
divyanshjain23 <divyansh.jain@joshtechnologygroup.com>
2025-07-22 15:47:54 +0530

    DJ_A2_10 : Corrected Typo's

--- src/components/List/List.component.tsx
+++ src/components/List/List.component.tsx
@@ -5,7 +5,7 @@ import { ListWrapperProps } from './List.types';
 
 /**
  * A wrapper around MUI's `List` component that renders content based on the provided items.
- * Accepts a list of items and toggle method as a parameter and handles their rendering of list items within the `List`.
+ * Accepts a list of items and toggle method as a parameter and handles  rendering of list items within the `List`.
  */
 const ListWrapper = ({
     items,
--- src/components/List/List.helper.tsx
+++ src/components/List/List.helper.tsx
@@ -4,7 +4,7 @@ import { ListItem, ListItemType } from '@components/List/ListItem';
  * Renders a list of `items` based on the provided data.
  *
  * @param {ListItemType[]} items - The list of items to render.
- * @param toggleSidebar - Method used toggle sidebar
+ * @param toggleSidebar - Method used to toggle sidebar
  * @returns {JSX.Element[]} An array of rendered `ListItem` components.
  */
 export function renderListItems(
